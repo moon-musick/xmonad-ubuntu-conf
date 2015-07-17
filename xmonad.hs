ec509ceb66a1aa502cf42530ce3eaff18f8fbd17
Łukasz Górski <moon.musick@gmail.com>
2015-07-17 19:50:58 +0200

    Change dmenu appearance

--- xmonad.hs
+++ xmonad.hs
@@ -211,7 +211,7 @@ myKeyBindings =
     ((myModMask, xK_b), sendMessage ToggleStruts)
     , ((myModMask, xK_a), sendMessage MirrorShrink)
     , ((myModMask, xK_z), sendMessage MirrorExpand)
-    , ((myModMask, xK_p), spawn "dmenu_run")
+    , ((myModMask, xK_p), spawn "dmenu_run -f -i -fn Inconsolata-11:normal -nb \\#93a1a1 -nf \\#002b36 -sb \\#859900 -sf \\#fdf6e3")
     , ((myModMask, xK_x), spawn "gnome-screenshot")
     , ((myModMask, xK_g), spawn "/home/lucas/bin/lock")
     , ((myModMask .|. mod1Mask, xK_space), spawn "synapse")
